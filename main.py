5aa042e7def955a079487d8de8720a85ec51795f
Klaus Raphael Machado <67723423+KlausMachado@users.noreply.github.com>
2025-03-12 09:56:16 -0300

    Update main.py
    update function names

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from registrationAnalyzer import load_records, add_record, update_record, delete_record, find_record, analyze_data, list_records
+from registrationAnalyzer import load_csv, add_person, update_person, delete_person, find_person, analyze_data, list_people
 
 # Menu principal
 def menu():
     try:
-        records = load_records()  # Carrega dados do arquivo
+        records = load_csv()  # Carrega dados do arquivo
     except Exception as e:
         print(f"Erro ao carregar registros: {e}")
         records = []  # Inicializa uma lista vazia em caso de erro
@@ -21,17 +21,17 @@ def menu():
 
         try:
             if option == '1':
-                add_record(records)  # Adiciona uma nova pessoa
+                add_person(records)  # Adiciona uma nova pessoa
             elif option == '2':
-                update_record(records)  # Altera os dados de uma pessoa
+                update_person(records)  # Altera os dados de uma pessoa
             elif option == '3':
-                delete_record(records)  # Apaga uma pessoa
+                delete_person(records)  # Apaga uma pessoa
             elif option == '4':
-                find_record(records)  # Busca uma pessoa
+                find_person(records)  # Busca uma pessoa
             elif option == '5':
                 analyze_data(records)  # Analisa os dados
             elif option == '6':
-                list_records(records)  # Lista todas as pessoas
+                list_people(records)  # Lista todas as pessoas
             elif option == '7':
                 break  
             else:
